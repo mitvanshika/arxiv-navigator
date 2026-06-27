89f8e9f20befc7f6860f74add0ad2498c14ff1d3
Vanhika Mittal <mitvanshika@gmail.com>
2026-06-27 17:26:17 +0530

    add README screenshots remove node_modules

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-  proxy: {
-    '/api': {
-      target: 'https://arxiv-navigator.onrender.com',
-      changeOrigin: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8002',
+        changeOrigin: true,
+      }
     }
   }
-}
+})
